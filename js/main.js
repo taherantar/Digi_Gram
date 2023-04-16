c0d81fedd10fc652c13813e39c99b7660ae2f97f
taherantar <taher.hp@hotmail.com>
2023-04-16 23:56:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// var next_btn = document.getElementById("next"),
+//     previous_btn = document.getElementById("previous"),
+//     slider_image = document.getElementById("sliderImage");
+
+// var images = ["blog-1.jpg", "blog-2.jpg", "blog-3.jpg"],
+//     root = "images/",
+//     counter = 0;
+// next_btn.onclick = function () {
+//     if (counter === images.length) {
+//         counter = 0;
+//     }
+//     counter++;
+//     slider_image.setAttribute("src", root + images[counter]);
+// };
+
+// previous_btn.onclick = function () {
+//     if (counter === 0) {
+//         counter = images.length;
+//     }
+//     counter--;
+//     slider_image.setAttribute("src", root + images[counter]);
+// };
+
+
